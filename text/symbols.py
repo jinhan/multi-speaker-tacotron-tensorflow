d430d1759c7590d8c6dd5af0ec0320a05758f46e
jinhan <choi.jinhan@gmail.com>
2018-01-04 15:50:19 +0900

    english support

---
+++ text/symbols.py
@@ -0,0 +1,23 @@
+'''
+Defines the set of symbols used in text input to the model.
+
+The default is a set of ASCII characters that works well for English or text that has been run
+through Unidecode. For other data, you can modify _characters. See TRAINING_DATA.md for details.
+'''
+from jamo import h2j, j2h
+from jamo.jamo import _jamo_char_to_hcj
+
+from .korean import ALL_SYMBOLS, PAD, EOS
+
+# symbols = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!\'(),-.:;? '
+#symbols = ALL_SYMBOLS
+
+from text import cmudict
+
+_characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!\'(),-.:;? '
+
+# Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
+_arpabet = ['@' + s for s in cmudict.valid_symbols]
+
+# Export all symbols:
+symbols = [PAD, EOS] + list(_characters) + _arpabet
